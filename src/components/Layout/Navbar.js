9c57ed893f5882c18d43de22244ade0fb4f34847
Yeondam Park <giyen9088@gmail.com>
2018-09-20 19:39:48 -0700

    adding github button on navbar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -13,6 +13,22 @@ class Navbar extends Component {
               <span className="logo">movieMatch</span>
             </a>
           </span>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item github-animated">
+              <a
+                href="https://github.com/yeondam88/moviematch"
+                className="nav-link"
+                style={{ color: "black", fontSize: "1em" }}
+              >
+                <span>
+                  <i
+                    className="fab fa-github"
+                    style={{ fontSize: "2em", color: "black" }}
+                  />
+                </span>
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     );
